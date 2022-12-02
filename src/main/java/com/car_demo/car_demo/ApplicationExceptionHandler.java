85c8e5d40c2087657c106f101b2bded781c5c1d1
jeremy <jeremy.j.holloway@gmail.com>
2022-12-02 08:45:25 -0500

    removed unused exception

--- src/main/java/com/car_demo/car_demo/ApplicationExceptionHandler.java
+++ src/main/java/com/car_demo/car_demo/ApplicationExceptionHandler.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.dao.DataIntegrityViolationException;
-//import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,16 +13,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import com.car_demo.car_demo.exception.CarNotFoundException;
 import com.car_demo.car_demo.exception.ErrorResponse;
 
 public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler {
-
-    @ExceptionHandler(CarNotFoundException.class)
-    public ResponseEntity<Object> handleCarNotFoundException(CarNotFoundException ex) {
-        ErrorResponse error = new ErrorResponse(Arrays.asList(ex.getLocalizedMessage()));  
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
     
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
@@ -35,7 +27,7 @@ public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler 
 
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
-        ErrorResponse error = new ErrorResponse(Arrays.asList("Data Integrity Violation: we cannot process your request."));  
+        ErrorResponse error = new ErrorResponse(Arrays.asList(ex.getLocalizedMessage()));  
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/car_demo/car_demo/exception/CarNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.car_demo.car_demo.exception;
-
-public class CarNotFoundException extends RuntimeException {
-    public CarNotFoundException(Long id) {
-        super("The id '" + String.valueOf(id) + "' does not exist in our records");
-    }
-}
