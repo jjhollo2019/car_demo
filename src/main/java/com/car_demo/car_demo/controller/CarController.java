e91d23fb12c786a53bc33d387d6367d3179b39ba
jeremy <jeremy.j.holloway@gmail.com>
2022-12-02 09:56:20 -0500

    removed import

--- src/main/java/com/car_demo/car_demo/controller/CarController.java
+++ src/main/java/com/car_demo/car_demo/controller/CarController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.validation.Valid;
 
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
