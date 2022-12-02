89f22cb45119afc255fb78beb3fff5ea80ebfd7d
jeremy <jeremy.j.holloway@gmail.com>
2022-12-02 08:57:10 -0500

    fixed exception handler to work across controllers

--- src/main/java/com/car_demo/car_demo/ApplicationExceptionHandler.java
+++ src/main/java/com/car_demo/car_demo/ApplicationExceptionHandler.java
@@ -5,16 +5,19 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.car_demo.car_demo.exception.ErrorResponse;
 
+@ControllerAdvice
 public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler {
     
     @Override
@@ -29,5 +32,11 @@ public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler 
     public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
         ErrorResponse error = new ErrorResponse(Arrays.asList(ex.getLocalizedMessage()));  
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public ResponseEntity<Object> handleDataAccessException(EmptyResultDataAccessException ex) {
+        ErrorResponse error = new ErrorResponse(Arrays.asList("Cannot delete non-existing resource"));  
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 }
